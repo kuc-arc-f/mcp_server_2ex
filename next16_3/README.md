a9c02f0e7d9d73ca6c440b481efc93ac510b4e86
中島浩二 <knaka0209@gmail.com>
2025-11-15 21:28:02 +0900

    up, 1115a15

---
+++ next16_3/README.md
@@ -0,0 +1,31 @@
+﻿# next16_3
+
+ Version: 0.9.1
+
+ Author  : 
+
+ date    : 2025/11/15 
+  
+ update  : 
+
+***
+
+next.js + Rust MCP Server , RAG Search
+
+***
+### Rust MCP Server
+
+https://github.com/kuc-arc-f/rust_3ex/tree/main/mcp_14
+
+***
+* dev-start
+```
+npm run build
+npm run dev
+```
+
+
+***
+### blog 
+
+***
---
+++ next16_3/app/api/chat/route.ts
@@ -0,0 +1,38 @@
+
+import { NextResponse } from "next/server";
+import RpcClient from '../../lib/RpcClient'
+const __dirname = process.cwd();
+const CMD_PATH = __dirname + "/dist/rust_mcp_server_14.exe"
+console.log("CMD_PATH=", CMD_PATH)
+
+export async function POST(req: Request) {
+  const retObj = {ret: 500, data: null}
+  try{  
+    const reqJson = await req.json();
+    console.log(reqJson);
+    const client = new RpcClient(CMD_PATH);
+    const resp = await client.call(
+      "tools/call", 
+      { 
+        name: "rag_search", 
+        arguments: {input_text: reqJson.messages}, 
+      },
+    );
+    client.close();  
+    console.log("resp:", resp);
+    //@ts-ignore
+    if(resp.content[0].text){
+      //const json = JSON.parse(resp.content[0].text)
+      //console.log(json);
+      retObj.ret = 200;
+      //@ts-ignore
+      retObj.data = resp.content[0].text;
+      return NextResponse.json(retObj);
+    }
+
+    return NextResponse.json(retObj);
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(retObj);
+  }
+}
